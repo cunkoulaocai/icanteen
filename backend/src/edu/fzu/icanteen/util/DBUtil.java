cbae8a12a55f485f212c8e15d0c58b793c157414
Reto <59349681+RETORETO@users.noreply.github.com>
2020-04-28 14:12:50 +0800

    Update DBUtil.java

--- backend/src/edu/fzu/icanteen/util/DBUtil.java
+++ backend/src/edu/fzu/icanteen/util/DBUtil.java
@@ -9,25 +9,25 @@ import java.sql.Statement;
 public class DBUtil {
 	static String ip = "127.0.0.1";
 	static int port = 3306;
-	static String database = "tmall";
+	static String database = "icanteen";
 	static String encoding = "UTF-8";
 	static String loginName = "root";
 	static String password = "123456";
 
 	static {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static Connection getConnection() throws SQLException {
-		String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
+		String url = String.format("jdbc:mysql://localhost:%d/%s?characterEncoding=%s&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true", port, database, encoding);
 		return DriverManager.getConnection(url, loginName, password);
 	}
 
-	/* ¹Ø±ÕÁ¬½ÓµÄ·½·¨ */
+	/* å…³é—­è¿žæŽ¥çš„æ–¹æ³• */
 	public static void close(ResultSet rs, Statement stmt, Connection conn) {
 		try {
 			if (rs != null)
