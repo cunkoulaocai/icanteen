02c7b841d5c09ae881f1732ac43f2f8f79608445
fzu-lxl <60782502+fzu-lxl@users.noreply.github.com>
2020-04-28 15:28:15 +0800

    商家DAO

---
+++ backend/src/edu/fzu/icanteen/dao/MerchantDAO.java
@@ -0,0 +1,19 @@
+package edu.fzu.icanteen.dao;
+
+import java.util.List;
+
+import edu.fzu.icanteen.pojo.Merchant;
+
+
+public interface MerchantDAO {
+	int getTotal();
+    void add(Merchant merchant);
+    void update(Merchant merchant);
+    void delete(int id) ;
+    Merchant get(int id);
+    List<Merchant> list();
+    List<Merchant> list(int start, int count) ;
+    boolean isExist(String name) ;
+    Merchant get(String name) ;
+    Merchant get(String name, String password) ;
+}
