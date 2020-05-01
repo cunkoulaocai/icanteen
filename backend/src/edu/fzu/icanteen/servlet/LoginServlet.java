1bb7dc5776dccb9f2a336c7382066ed99239432e
Reto <59349681+RETORETO@users.noreply.github.com>
2020-05-01 15:26:44 +0800

    Update LoginServlet.java

--- backend/src/edu/fzu/icanteen/servlet/LoginServlet.java
+++ backend/src/edu/fzu/icanteen/servlet/LoginServlet.java
@@ -24,21 +24,21 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		System.out.println("request--->"+request.getRequestURL()+"===="+request.getParameterMap().toString());
-		String studentId = request.getParameter("studentId"); // 获取客户端传过来的参数
+		// 获取客户端传过来的参数
+		String studentId = request.getParameter("studentId"); 
 		String password = request.getParameter("password");
 		response.setContentType("text/html;charset=utf-8");
 		
 		if (studentId == null || studentId.equals("") || password == null || password.equals("")) {
 			System.out.println("用户名或密码为空");
 			return;
-		} // 请求数据库
-		// 打开数据库连接
+		}
+
 		CustomerDAO customerDAO = new CustomerDAOImpl();
 		Customer customer = customerDAO.get(studentId, password);
 		BaseBean data = new BaseBean(); // 基类对象，回传给客户端的json对象
 		
 		if (customer != null) {
-			// 判断账号是否存在
 			data.setCode(1);
 			data.setData(customer);
 			data.setMsg("登陆成功");
@@ -46,15 +46,17 @@ public class LoginServlet extends HttpServlet {
 			data.setMsg("登陆失败，用户名或密码错误");
 		}
 		Gson gson = new Gson();
-		String json = gson.toJson(data);
 		// 将对象转化成json字符串
+		String json = gson.toJson(data);
+		
 		try {
-			response.getWriter().println(json);
 			// 将json数据传给客户端
+			response.getWriter().println(json);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			response.getWriter().close(); // 关闭这个流，不然会发生错误的
+			// 关闭这个流
+			response.getWriter().close();
 		}
 	}
 	
