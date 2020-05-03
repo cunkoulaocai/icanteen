31dcae01175f8b11979d4f49cd482affe3b67f7a
BluEyeDragonX <346625116@qq.com>
2020-05-03 22:42:46 +0800

    完成商家端我的页面

---
+++ merchant/app/src/main/java/com/example/merchant/ui/MyPage.java
@@ -0,0 +1,36 @@
+package com.example.merchant.ui;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.merchant.R;
+import com.example.merchant.ui.login.LoginViewModel;
+import com.example.merchant.ui.login.LoginViewModelFactory;
+
+public class MyPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+
+}
